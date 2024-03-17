d564b5868e508c6b085b76c29805a41cfa1e39bf
khaingsuthway <khaingsuthway@gmail.com>
2024-03-17 18:37:23 +0700

    Update scanning record module

--- scanning_window.py
+++ scanning_window.py
@@ -94,18 +94,11 @@ def scan_without_interval(record, window_width):
         pac_percentages.append(pac_percentage)
         pvc_percentage = segment_beat_annotation_count.get('V', 0) / total_count * 100
         pvc_percentages.append(pvc_percentage)
-
         
-        if is_NSR(record._Record__label, pac_percentage, pvc_percentage):
-            true_class.append('NSR')
-        elif is_PAC(record._Record__label, pac_percentage, pvc_percentage):
-            true_class.append('PAC')
-        elif is_PVC(record._Record__label, pac_percentage, pvc_percentage):
-            true_class.append('PVC')
-        elif is_AF(record._Record__label, pac_percentage, pvc_percentage):
-            true_class.append('AF')
-        else:
-            true_class.append('Others')
+        true_class.append(determine_true_class(record._Record__label,
+                                               pac_percentage,
+                                               pvc_percentage))       
+        
 
         left_end += window_step
         right_end = left_end + int(window_width * sampfreq)
@@ -129,7 +122,126 @@ def scan_without_interval(record, window_width):
 
     return data_within_window
 
+def scan_with_interval(record, window_width):
+    sampfreq = record._Record__sf
+    signal = record._Record__signal
+    heart_rate = calculate_bpm(signal, sampfreq)
+    heart_cycle = heart_rate / 60
+    
+    types_of_step = input("Choose 'bpm' or 'sec': ")
+    no_of_beats_per_step = int(input("Give number of steps: "))
+    
+    if types_of_step == 'bpm':
+        window_step = int((no_of_beats_per_step * heart_cycle) * sampfreq)
+    elif types_of_step == 'sec':
+        window_step = int(no_of_beats_per_step * sampfreq)
+        
+    
+    nsr_interval = record.get_valid_rhythm_interval(duration=window_width, type='NSR') 
+    print(f"NSR interval is from {nsr_interval}")
+    af_interval = record.get_valid_rhythm_interval(duration=window_width, type='AF') 
+    print(f"AF interval is from {af_interval}")
+    
+    def process_interval(valid_interval):
+        
+        ecg_signals = []
+        beat_annotations = []
+        beat_annotated_points = []
+        pac_percentages = []
+        pvc_percentages = []
+        true_class = []
+        # Ensure valid_interval is array-like
+        if isinstance(valid_interval, (list, tuple)) and len(valid_interval) > 0:
+                        
+            left_end = valid_interval[0][0]
+            print(left_end)
+            right_end = left_end+(window_width * sampfreq)            
+            
+            while right_end <= valid_interval[0][1]:
+                # Process the interval
+                signal_within_window = record._Record__signal[left_end:right_end]                
+                ecg_signals.append(signal_within_window)
 
+                annotated_index = np.intersect1d(np.where(left_end <= record._Record__sample),
+                                  np.where(right_end >= record._Record__sample))   
+                print(annotated_index)             
+
+                symbol_within_window = [record._Record__symbol[i] for i in annotated_index]
+                beat_annotations.append(symbol_within_window)
+
+                sample_within_window = [record._Record__sample[i] - left_end for i in annotated_index]
+                beat_annotated_points.append(sample_within_window)
+
+                # Calculate percentages and true class
+                beats, count = np.unique(symbol_within_window, return_counts=True)
+                segment_beat_annotation_count = dict(zip(beats, count))
+                total_count = sum(segment_beat_annotation_count.values())
+                
+                pac_percentage = segment_beat_annotation_count.get('A', 0) / total_count * 100
+                pac_percentages.append(pac_percentage)
+                pvc_percentage = segment_beat_annotation_count.get('V', 0) / total_count * 100
+                pvc_percentages.append(pvc_percentage)
+                
+                true_class.append(determine_true_class(record._Record__label,
+                                                    pac_percentage,
+                                                    pvc_percentage)) 
+                
+
+                left_end += window_step
+                print(f"left_end pt: {left_end}")
+                right_end = left_end + int(window_width * sampfreq)
+            
+        else:
+            # Handle case when valid_interval is not array-like
+            print("Invalid interval:", valid_interval)
+            # Optionally, you can perform alternative actions here
+        
+        parent_record_repeated = [record._Record__parent] * len(ecg_signals)
+        label_repeated = [record._Record__label] * len(ecg_signals)
+        heart_rate_repeated = [heart_rate] * len(ecg_signals)
+
+        data_within_window = pd.DataFrame({'parent_record': parent_record_repeated,
+                                        'signals': ecg_signals,
+                                        'beat_annotation_symbols': beat_annotations,
+                                        'annotated_samples': beat_annotated_points,
+                                        'pac_percent': pac_percentages,
+                                        'pvc_percent': pvc_percentages,
+                                        'avg_heart_rate': heart_rate_repeated,
+                                        'label': label_repeated,
+                                        'true_class': true_class})
+
+        return data_within_window
+        
+    # Process AF interval
+    data_within_af_interval = pd.DataFrame()
+    if len(af_interval):
+        data_within_af_interval = process_interval(af_interval)
+
+    # Process NSR interval
+    data_within_nsr_interval = pd.DataFrame()
+    if len(nsr_interval):
+        data_within_nsr_interval = process_interval(nsr_interval)
+
+    # Concatenate data from both intervals
+    data_within = pd.concat([data_within_af_interval, data_within_nsr_interval])
+
+    print(f"There are {data_within.shape[0]} segments in the record.")
+    
+    return data_within
+
+
+def determine_true_class(label, pac_percentage, pvc_percentage):
+    if is_NSR(label, pac_percentage, pvc_percentage):
+        return 'NSR'
+    elif is_PAC(label, pac_percentage, pvc_percentage):
+        return 'PAC'
+    elif is_PVC(label, pac_percentage, pvc_percentage):
+        return 'PVC'
+    elif is_AF(label, pac_percentage, pvc_percentage):
+        return 'AF'
+    else:
+        return 'Others'
+    
 def is_AF(label, pac_percentage, pvc_percentage):
     return label != 'non atrial fibrillation' and pac_percentage == 0 and pvc_percentage == 0
 
@@ -142,95 +254,6 @@ def is_PAC(label, pac_percentage, pvc_percentage):
 def is_PVC(label, pac_percentage, pvc_percentage):
     return label == 'non atrial fibrillation' and pac_percentage == 0 and pvc_percentage >= 20
 
-
-def scan_with_interval(record,window_width):
-    
-    
-    sampfreq = record._Record__sf
-    
-    heart_rate = calculate_bpm(signal, sampfreq)
-    heart_cycle = heart_rate / 60
-    types_of_step = input("Choose 'bpm' or 'sec': ")
-    no_of_beats_per_step = int(input("Give number of steps: "))
-
-    if types_of_step == 'bpm':
-        window_step = int((no_of_beats_per_step * heart_cycle) * sampfreq)
-    elif types_of_step == 'sec':
-        window_step = int(no_of_beats_per_step * sampfreq)
-
-    ecg_signals = []
-    beat_annotations = []
-    beat_annotated_points = []
-    pac_percentages = []
-    pvc_percentages = []
-    true_class = []
-    
-    valid_interval=record.get_valid_rhythm_interval(duration=window_width)
-    signal = record._Record__signal[valid_interval[0][0]:valid_interval[]]
-    symbol = record._Record__symbol
-    sample = record._Record__sample
-    
-    left_end=valid_interval[0][0]
-    right_end=window_width*record._Record__sf
-    
-    while right_end <= valid_interval[0][1]:  
-        print("left_end:", left_end)
-        print("right_end:", right_end)
-        print("window_width:", window_width)
-    
-        signal_within_window = signal[left_end:right_end]
-        ecg_signals.append(signal_within_window)
-
-        annotated_index = np.intersect1d(np.where(left_end <= sample),
-                                            np.where(right_end >= sample))
-        symbol_within_window = [symbol[i] for i in annotated_index]
-        beat_annotations.append(symbol_within_window)
-
-        sample_within_window = [sample[i] - left_end for i in annotated_index]
-        beat_annotated_points.append(sample_within_window)
-
-        beats, count = np.unique(symbol_within_window, return_counts=True)
-        segment_beat_annotation_count = dict(zip(beats, count))
-        total_count = sum(segment_beat_annotation_count.values())
-
-        pac_percentage = segment_beat_annotation_count.get('A', 0) / total_count * 100
-        pac_percentages.append(pac_percentage)
-        pvc_percentage = segment_beat_annotation_count.get('V', 0) / total_count * 100
-        pvc_percentages.append(pvc_percentage)
-
-        
-        if is_NSR(record._Record__label, pac_percentage, pvc_percentage):
-            true_class.append('NSR')
-        elif is_PAC(record._Record__label, pac_percentage, pvc_percentage):
-            true_class.append('PAC')
-        elif is_PVC(record._Record__label, pac_percentage, pvc_percentage):
-            true_class.append('PVC')
-        elif is_AF(record._Record__label, pac_percentage, pvc_percentage):
-            true_class.append('AF')
-        else:
-            true_class.append('Others')
-
-        left_end += window_step
-        right_end = left_end + int(window_width * sampfreq)
-        
-
-    parent_record_repeated = [record._Record__parent] * len(ecg_signals)
-    label_repeated = [record._Record__label] * len(ecg_signals)
-    heart_rate_repeated = [heart_rate] * len(ecg_signals)
-
-    data_within_window = pd.DataFrame({'parent_record': parent_record_repeated,
-                                    'signals': ecg_signals,
-                                    'beat_annotation_symbols': beat_annotations,
-                                    'annotated_samples': beat_annotated_points,
-                                    'pac_percent': pac_percentages,
-                                    'pvc_percent': pvc_percentages,
-                                    'avg_heart_rate': heart_rate_repeated,
-                                    'label': label_repeated,
-                                    'true_class': true_class})
-
-    print(f"There are {data_within_window.shape[0]} segments in the record.")
-
-    return data_within_window
         
         
     
