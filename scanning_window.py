6a30220d555a438039fcdf8a56703fe46e4c5fa9
khaingsuthway <khaingsuthway@gmail.com>
2024-05-27 00:43:46 +0700

    Modify arr-type definition

--- scanning_window.py
+++ scanning_window.py
@@ -27,9 +27,7 @@ def calculate_bpm(signal, sampfreq) -> int:
     return int(heart_rate)
 
 def scan_record(record, window_width, window_step=None):
-    # signal = record._Record__signal
-    # symbol = record._Record__symbol
-    # sample = record._Record__sample
+    
     rhythm_annotation = record._Record__aux
 
     rhythm_keys = Counter(rhythm_annotation).keys()
@@ -39,7 +37,7 @@ def scan_record(record, window_width, window_step=None):
                                                    window_width=window_width,
                                                    )
     else:
-        print(f"There's rhythm annotation. {rhythm_keys}")
+        print(f"There's rhythm annotation. {rhythm_keys} in {record._Record__parent}")
         data_within_window=scan_with_interval(record=record,window_width=window_width)
         
 
@@ -55,8 +53,8 @@ def scan_without_interval(record, window_width):
     right_end = int(window_width * sampfreq)
     heart_rate = calculate_bpm(signal, sampfreq)
     heart_cycle = heart_rate / 60
-    types_of_step = 'bpm' #input("Choose 'bpm' or 'sec': ")
-    no_of_beats_per_step = 1 #int(input("Give number of steps: "))
+    types_of_step = 'sec' #input("Choose 'bpm' or 'sec': ")
+    no_of_beats_per_step = 30 #int(input("Give number of steps: "))
 
     if types_of_step == 'bpm':
         window_step = int((no_of_beats_per_step * heart_cycle) * sampfreq)
@@ -97,8 +95,9 @@ def scan_without_interval(record, window_width):
         
         true_class.append(determine_true_class(record._Record__label,
                                                pac_percentage,
-                                               pvc_percentage))       
-        
+                                               pvc_percentage))     
+
+              
 
         left_end += window_step
         right_end = left_end + int(window_width * sampfreq)
@@ -119,7 +118,7 @@ def scan_without_interval(record, window_width):
                                        'true_class': true_class})
     data_within_window=pd.concat([signal,info],axis=1)
 
-    print(f"There are {data_within_window.shape[0]} segments in the record.")
+    print(f"There are {data_within_window.shape[0]} segments in the record {record._Record__parent}.")
 
     return data_within_window
 
@@ -129,8 +128,8 @@ def scan_with_interval(record, window_width):
     heart_rate = calculate_bpm(signal, sampfreq)
     heart_cycle = heart_rate / 60
     
-    types_of_step = 'bpm'#input("Choose 'bpm' or 'sec': ")
-    no_of_beats_per_step = 1 #int(input("Give number of steps: "))
+    types_of_step = 'sec'#input("Choose 'bpm' or 'sec': ")
+    no_of_beats_per_step = 30 #int(input("Give number of steps: "))
     
     if types_of_step == 'bpm':
         window_step = int((no_of_beats_per_step * heart_cycle) * sampfreq)
@@ -147,8 +146,12 @@ def scan_with_interval(record, window_width):
     if len(af_interval):
         af_interval=record.get_valid_rhythm_interval(duration=window_width, type='AF') 
         print(f"AF interval is from {af_interval}")
+
+    if not af_interval and not nsr_interval:
+        print ("There is no AF and NSR longer than 30 second segment")
+        return pd.DataFrame()
     
-    def process_interval(valid_interval):
+    def process_interval(valid_interval,interval_name):
         
         ecg_signals = []
         beat_annotations = []
@@ -156,6 +159,7 @@ def scan_with_interval(record, window_width):
         pac_percentages = []
         pvc_percentages = []
         true_class = []
+        label=interval_name
         # Ensure valid_interval is array-like
         if isinstance(valid_interval, (list, tuple)) and len(valid_interval) > 0:
                       
@@ -164,37 +168,36 @@ def scan_with_interval(record, window_width):
                 right_end = left_end+(window_width * sampfreq)            
                 
                 while right_end <= interval[1]:
-                    # Process the interval
-                    signal_within_window = record._Record__signal[left_end:right_end]          
-                    ecg_signals.append(signal_within_window)
-
+                    # Process the interval      
+                    signal_within_window = record._Record__signal[left_end:right_end] 
+                    
                     annotated_index = np.intersect1d(np.where(left_end <= record._Record__sample),
-                                    np.where(right_end >= record._Record__sample))   
-                                 
+                                    np.where(right_end >= record._Record__sample))                                    
 
-                    symbol_within_window = [record._Record__symbol[i] for i in annotated_index]
-                    beat_annotations.append(symbol_within_window)
+                    symbol_within_window = [record._Record__symbol[i] for i in annotated_index]                    
 
                     sample_within_window = [record._Record__sample[i] - left_end for i in annotated_index]
-                    beat_annotated_points.append(sample_within_window)
-
+                    
                     # Calculate percentages and true class
                     beats, count = np.unique(symbol_within_window, return_counts=True)
                     segment_beat_annotation_count = dict(zip(beats, count))
                     total_count = sum(segment_beat_annotation_count.values())
-                    
-                    pac_percentage = segment_beat_annotation_count.get('A', 0) / total_count * 100
-                    pac_percentages.append(pac_percentage)
-                    pvc_percentage = segment_beat_annotation_count.get('V', 0) / total_count * 100
-                    pvc_percentages.append(pvc_percentage)
-                    
-                    true_class.append(determine_true_class(record._Record__label,
-                                                        pac_percentage,
-                                                        pvc_percentage)) 
-                    
+                    if total_count:
+                        pac_percentage = segment_beat_annotation_count.get('A', 0) / total_count * 100
+                        pac_percentages.append(pac_percentage)
+                        pvc_percentage = segment_beat_annotation_count.get('V', 0) / total_count * 100
+                        pvc_percentages.append(pvc_percentage)
+                        
+                        true_class.append(determine_true_class(label,
+                                                            pac_percentage,
+                                                            pvc_percentage)) 
+                                 
+                        ecg_signals.append(signal_within_window)
+                        beat_annotations.append(symbol_within_window)
+                        beat_annotated_points.append(sample_within_window)
+                        
 
-                    left_end += window_step
-                    print(f"left_end pt: {left_end}")
+                    left_end += int(window_width * sampfreq)                    
                     right_end = left_end + int(window_width * sampfreq)
                 
         else:
@@ -203,9 +206,9 @@ def scan_with_interval(record, window_width):
             # Optionally, you can perform alternative actions here
         
         parent_record_repeated = [record._Record__parent] * len(ecg_signals)
-        label_repeated = [record._Record__label] * len(ecg_signals)
+        label_repeated = [label] * len(ecg_signals)       
+    
         heart_rate_repeated = [heart_rate] * len(ecg_signals)
-
         signal= pd.DataFrame(ecg_signals)
         info = pd.DataFrame({'parent_record': parent_record_repeated,                                        
                                         'beat_annotation_symbols': beat_annotations,
@@ -222,12 +225,21 @@ def scan_with_interval(record, window_width):
     # Process AF interval
     data_within_af_interval = pd.DataFrame()
     if len(af_interval):
-        data_within_af_interval = process_interval(af_interval)
+        if record._Record__label:
+            data_within_af_interval = process_interval(af_interval,record._Record__label)
+        else:
+            data_within_af_interval = process_interval(af_interval,'atrial fibrillation')
+            
 
     # Process NSR interval
     data_within_nsr_interval = pd.DataFrame()
     if len(nsr_interval):
-        data_within_nsr_interval = process_interval(nsr_interval)
+        if record._Record__label:
+            data_within_nsr_interval = process_interval(nsr_interval,record._Record__label)
+        else:
+            data_within_nsr_interval = process_interval(nsr_interval,'non atrial fibrillation')
+            
+
 
     # Concatenate data from both intervals
     if len(data_within_af_interval) and len(data_within_nsr_interval):
